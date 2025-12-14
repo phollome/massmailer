bdae1bcc7bd4c3f81631daebd872875f1b13677f
Peter Holló <hello@songsforthe.dev>
2025-12-14 19:27:50 +0100

    refactor: use tab nav

--- app/routes/account/$accountId.tsx
+++ app/routes/account/$accountId.tsx
@@ -1,8 +1,11 @@
 import { invariantResponse } from "@epic-web/invariant";
+import { Link as LinkStyle, Strong, TabNav, Text } from "@radix-ui/themes";
 import {
+  Link,
   NavLink,
   Outlet,
   useLoaderData,
+  useLocation,
   type LoaderFunctionArgs,
 } from "react-router";
 import prisma from "~/db.server";
@@ -22,14 +25,24 @@ export async function loader(args: LoaderFunctionArgs) {
 
 function Account() {
   const loaderData = useLoaderData<typeof loader>();
+  const location = useLocation();
   return (
     <>
-      <div>
-        {loaderData.account.email}
-        <NavLink to="./mails">Mails</NavLink>
-        <NavLink to="./contacts">Contacts</NavLink>
-        <Outlet />
-      </div>
+      <Text as="p" size="1" align="right">
+        Selected account: <Strong>{loaderData.account.email}</Strong>{" "}
+        <LinkStyle asChild>
+          <Link to="/">(Back to selection)</Link>
+        </LinkStyle>
+      </Text>
+      <TabNav.Root size="2">
+        <TabNav.Link asChild active={location.pathname.includes("/mails")}>
+          <NavLink to="./mails">Mails</NavLink>
+        </TabNav.Link>
+        <TabNav.Link asChild active={location.pathname.includes("/contacts")}>
+          <NavLink to="./contacts">Contacts</NavLink>
+        </TabNav.Link>
+      </TabNav.Root>
+      <Outlet />
     </>
   );
 }
